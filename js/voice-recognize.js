2626d6c06d008444ac62f0ebe622c1a6cdf46694
André Maia <76632983+andre-maia51@users.noreply.github.com>
2024-01-31 11:11:45 -0300

    Update voice-recognize.js

--- js/voice-recognize.js
+++ js/voice-recognize.js
@@ -1,4 +1,5 @@
 const elementoChute = document.querySelector('#chute')
+let cont = 0
 
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
@@ -11,13 +12,20 @@ recognition.addEventListener('result', (e) => {
     const value = e.results[0][0].transcript
     exibeChute(value)
     verificaChute(value)
+    calculaTentativas()
 })
 
 function exibeChute(value) {
     elementoChute.innerHTML = `
         <div>Você disse:</div>
         <span class="box">${value}</span>
+    `
+}
 
+function calculaTentativas() {
+    cont++;
+    elementoChute.innerHTML += `
+        <div class="text-tentativa">Tentativas: ${cont}</div>
     `
 }
 
